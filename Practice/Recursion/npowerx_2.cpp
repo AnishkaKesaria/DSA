5b6a6474a20b53349d152b64470904b13a46e39e
Anishka Kesaria <kescotak@gmail.com>
2022-12-25 20:43:53 +0530

    Added codes

---
+++ Practice/Recursion/npowerx_2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(int n, int x)
+{
+    if(x==1) return n;
+    if(x==0) return 1;
+    int ans;
+    int pans = power(n, x/2);
+    ans = pans*pans;
+    if(x%2==1) ans*=n;
+    return ans;
+}
+
+int main()
+{
+    int n,x;
+    cin>>n>>x;
+    cout<<power(n,x);
+    return 0;
+}
