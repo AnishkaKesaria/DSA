9aed2e3ab21d82509256716ae9e21b603b8affe3
Anishka Kesaria <kescotak@gmail.com>
2023-01-08 16:26:02 +0530

    Added codes

---
+++ Love Babbar/2D Arrays/seachin2D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPresent(int arr[][], int target, int i, int j)
+{
+    for(int )
+}
+
+int main()
+{
+    int i,j;
+    cin>>i>>j;
+    int arr[i][j];
+    for(int row = 0; row<i; row++)
+    {
+        for(int col = 0; col<j; col++)
+        {
+            cin>>arr[row][col];
+        }
+    }
+
+    int target;
+    cout<<"Enter target: ";
+    if(isPresent(arr,target,i,j))
+        cout<<"Element Found;"
+    else cout<<"Element not found";
+    return 0;
+}
